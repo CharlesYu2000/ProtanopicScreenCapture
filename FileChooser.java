4486e438c22f1fbab0ffbf200fd7cc07717c6e0b
bphanboi <bnp009@ucsd.edu>
2018-10-13 12:19:58 -0700

    Update FileChooser.java

---
+++ FileChooser.java
@@ -0,0 +1,31 @@
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import java.util.Properties;
+import java.io.*;
+import java.net.*;
+
+public class FileChooser {
+
+    private static Properties appProperties = null;
+
+    private static final String MEDIA_DIRECTORY = "mediaDirectory";
+
+    private static final String PROPERTY_FILE_NAME =
+        "SimplePictureProperties.txt";
+
+    public static String getMediaPath(String fileName) {
+
+        
+
+
+    return "hello";
+
+
+
+    }
+
+
+
+
+
+}
