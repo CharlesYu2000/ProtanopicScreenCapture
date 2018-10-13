ab4183b43a8552a6e7c6780b04bbbcb260b6398c
Andrew Tang <andrewtang9@gmail.com>
2018-10-13 11:22:00 -0700

    Used robot to grab pixels

--- ScreenGrab.java
+++ ScreenGrab.java
@@ -1,2 +1,23 @@
-import java.awt.Toolkit.*;
-import java.awt.
+import java.awt.*;
+import java.awt.image.*;
+
+public class ScreenGrab {
+
+    public static void main(String[] args) throws AWTException{
+        Robot robot = new Robot();
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+        //Rectangle screenRect = new Rectangle(size.width, size.height);
+        //BufferedImage image = robot.createScreenCapture(screenRect);
+        int[][] rgb = new int[size.width][size.height];
+        Color color;
+        System.out.println(size.width+ ", " +size.height);
+        for(int x = 0; x < size.width; x++){
+            for(int y = 0; y < size.height; y++){
+                color = robot.getPixelColor(x, y);
+                rgb[x][y] = color.getRGB();
+            }
+        }
+        //System.out.println(rgb);
+    }
+
+}
