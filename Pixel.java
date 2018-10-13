839a34c7635a6c44896dcffb2920348556f9516c
Charles Yu <CharlesYu2000@gmail.com>
2018-10-13 11:29:36 -0700

    Changed tabs to spaces oops lol

--- Pixel.java
+++ Pixel.java
@@ -2,96 +2,96 @@ import java.awt.Color;
 
 public class Pixel{
 
-	protected Picture pic;
+    protected Picture pic;
 
-	private int x;
+    private int x;
 
-	private int y;
+    private int y;
 
-	public Pixel(Picture pic, int x, int y){
-		this.pic = pic;
-		this.x = x;
-		this.y = y;
-	}
+    public Pixel(Picture pic, int x, int y){
+        this.pic = pic;
+        this.x = x;
+        this.y = y;
+    }
 
-	public int getX(){
-		return x;
-	}
+    public int getX(){
+        return x;
+    }
 
-	public int getY(){
-		return y;
-	}
+    public int getY(){
+        return y;
+    }
 
-	public int getAlpha(){
-		return ( (pic.getPixel(x, y) >> 24) & 0xff);
-	}
+    public int getAlpha(){
+        return ( (pic.getPixel(x, y) >> 24) & 0xff);
+    }
 
-	public int getRed(){
-		return ( (pic.getPixel(x, y) >> 16) & 0xff );
-	}
+    public int getRed(){
+        return ( (pic.getPixel(x, y) >> 16) & 0xff );
+    }
 
-	public int getGreen(){
-		return ( (pic.getPixel(x, y) >> 8) & 0xff);
-	}
+    public int getGreen(){
+        return ( (pic.getPixel(x, y) >> 8) & 0xff);
+    }
 
-	public int getBlue(){
-		return (pic.getPixel(x, y) & 0xff);
-	}
+    public int getBlue(){
+        return (pic.getPixel(x, y) & 0xff);
+    }
 
-	public static int rgbBoundsCheck(int value){
-		if(value<0){
-			value = 0;
-		}else if(value>255){
-			value = 255;
-		}
+    public static int rgbBoundsCheck(int value){
+        if(value<0){
+            value = 0;
+        }else if(value>255){
+            value = 255;
+        }
 
-		return value;
-	}
+        return value;
+    }
 
-	public void setAlpha(int newAlpha){
-		int alpha = boundsCheck(newAlpha);
-		int rgb = (alpha << 24) + (getRed() << 16) 
-					+ (getGreen() << 8) + getBlue();
-		pic.setPixel(x, y, rgb);
-	}
+    public void setAlpha(int newAlpha){
+        int alpha = boundsCheck(newAlpha);
+        int rgb = (alpha << 24) + (getRed() << 16) 
+                    + (getGreen() << 8) + getBlue();
+        pic.setPixel(x, y, rgb);
+    }
 
-	public void setRed(int newRed){
-		int red = boundsCheck(newRed);
-		int rgb = (getAlpha() << 24) + (red << 16) 
-					+ (getGreen() << 8) + getBlue();
-		pic.setPixel(x, y, rgb);
-	}
+    public void setRed(int newRed){
+        int red = boundsCheck(newRed);
+        int rgb = (getAlpha() << 24) + (red << 16) 
+                    + (getGreen() << 8) + getBlue();
+        pic.setPixel(x, y, rgb);
+    }
 
-	public void setGreen(int newGreen){
-		int green = boundsCheck(newGreen);
-		int rgb = (getAlpha() << 24) + (getRed() << 16) 
-					+ (green << 8) + getBlue();
-		pic.setPixel(x, y, rgb);
-	}
+    public void setGreen(int newGreen){
+        int green = boundsCheck(newGreen);
+        int rgb = (getAlpha() << 24) + (getRed() << 16) 
+                    + (green << 8) + getBlue();
+        pic.setPixel(x, y, rgb);
+    }
 
-	public void setBlue(int newBlue){
-		int blue = boundsCheck(newBlue);
-		int rgb = (getAlpha() << 24) + (getRed() << 16) 
-					+ (getGreen() << 8) + blue;
-		pic.setPixel(x, y, rgb);
-	}
+    public void setBlue(int newBlue){
+        int blue = boundsCheck(newBlue);
+        int rgb = (getAlpha() << 24) + (getRed() << 16) 
+                    + (getGreen() << 8) + blue;
+        pic.setPixel(x, y, rgb);
+    }
 
-	public Color getColor(){
-		return new Color(getRed(), getGreen(), getBlue());
-	}
+    public Color getColor(){
+        return new Color(getRed(), getGreen(), getBlue());
+    }
 
-	public void setColor(Color color){
-		int red = color.getRed();
-		int green = color.getGreen();
-		int blue = color.getBlue();
-		int rgb = (getAlpha() << 24) + (red << 16) 
-					+ (green << 8) + blue;
-		pic.setPixel(x, y, rgb);
-	}
+    public void setColor(Color color){
+        int red = color.getRed();
+        int green = color.getGreen();
+        int blue = color.getBlue();
+        int rgb = (getAlpha() << 24) + (red << 16) 
+                    + (green << 8) + blue;
+        pic.setPixel(x, y, rgb);
+    }
 
-	public String toString(){
-		return "Pixel at " + pic.title + "'s (" + x + ", " + y + "); " + 
-				"ARGB is (" + getAlpha() + ", " + getRed() + ", " +
-				getGreen() + ", " + getBlue() + ").";
-	}
+    public String toString(){
+        return "Pixel at " + pic.title + "'s (" + x + ", " + y + "); " + 
+                "ARGB is (" + getAlpha() + ", " + getRed() + ", " +
+                getGreen() + ", " + getBlue() + ").";
+    }
 }
