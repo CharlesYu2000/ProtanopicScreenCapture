2bee5257986d1d8c46dc12783616cbfc6d926ff2
Andrew Tang <andrewtang9@gmail.com>
2018-10-13 08:50:49 -0700

    Added constructor

--- Frame.java
+++ Frame.java
@@ -8,12 +8,24 @@ public class Frame {
 
     JLabel label = new JLabel(imageIcon);
 
+    JSlider slider = new JSlider();
+
     private Picture picture;
 
     public Frame() {
-    if (picture != null) {
+      frame.getContentPane().add(label);
+      frame.pack();
+      frame.setVisible(true);
+    }
+
+    public Frame(Picture pic) {
+      this.picture = pic;
+      if(pic == null){
+        throw new NullPointerException();
+      }
       imageIcon.setImage(picture.getImage());
-      frame.setTitle(picture.getTitle());}
+      frame.setTitle(picture.getTitle());
+
       frame.getContentPane().add(label);
       frame.pack();
       frame.setVisible(true);
