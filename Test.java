58f0190a46705e897f12aeab98d93aa270b8cb91
bphanboi <bnp009@ucsd.edu>
2018-10-13 11:22:31 -0700

    Update Test.java

---
+++ Test.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.awt.Toolkit;
+
+public class Test {
+
+    public static void main(String[] args) throws AWTException {
+        Robot capture = new Robot();
+        BufferedImage curr;
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int)screenSize.getWidth();
+        int height = (int)screenSize.getHeight();
+        Rectangle screenRect = new Rectangle(width, height);
+
+        for(int i = 0; i < 24; i++) {
+            curr = capture.createScreenCapture(screenRect);
+            for(int j = 0; j < width; j++) {
+                for(int k = 0; k < height; k++) {
+                    curr.getRGB(j, k);
+                }
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
