dce4adb81185cf9b5a643a28e02d26496fd799e1
Andrew Tang <andrewtang9@gmail.com>
2018-10-13 01:24:44 -0700

    Added Frame.java

---
+++ Frame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame{
+    JFrame frame = new JFrame();
+
+    ImageIcon imageIcon = new ImageIcon();
+
+    JLabel label = new JLabel(imageIcon);
+
+    
+}
